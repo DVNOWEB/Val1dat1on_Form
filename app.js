e45fbac67995f57016b99dca7778c5d54cfe1018
DVNO <dvno@live.se>
2022-10-19 10:49:01 +0200

    100%

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ class UI {
     // row.innerHTML = `<p>Hello, ${user.firstName} ${user.lastName} you are successfully registered!</p>`
     // card.appendChild(row)
 
-
   }
 
   showAlert(message, className){
@@ -30,19 +29,17 @@ class UI {
     div.className = `alert ${className}`
     // add text
     div.appendChild(document.createTextNode(message))
-    // Get parent
-    //const container = document.querySelector('.wrapper');
-    const backMessage = document.querySelector('#backMessage')
+    // Feedback message
+    const errorMessage = document.querySelector('#errorMessage');
     // Get form
     const form = document.querySelector('#validationForm')
     // insert alert
-    //container.insertBefore(div, form);
-    backMessage.appendChild(div, form)
+    errorMessage.appendChild(div, form)
     
 
     // Timeout
     setTimeout(function(){
-    document.querySelector('.alert').remove()}, 3000)
+    document.querySelector('.alert').remove()}, 4000)
 
  }
 
@@ -82,76 +79,61 @@ function validCheckbox(){
   
 }
 
-// Error message footer
-function errorCheck() {
-  let errorMsg = document.getElementById('errorMessage')
-  if (errorMsg.style.display === 'none') {
-    errorMsg.style.display = 'block';
-  } else {
-    errorMsg.style.display = 'none';
-  }
-}
-
 // Event listeners
 document.getElementById('validationForm').addEventListener('submit', function(e){
 
    // console.log('test')
 
-    const firstName = document.getElementById('firstName').value
-    const lastName = document.getElementById('lastName').value
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    const repPassword = document.getElementById('repeatPassword').value
-    const checkbox = document.getElementById('terms').checked
-
+    const firstName = document.getElementById('firstName').value,
+          lastName = document.getElementById('lastName').value,
+          email = document.getElementById('email').value,
+          password = document.getElementById('password').value,
+          repPassword = document.getElementById('repeatPassword').value,
+          checkbox = document.getElementById('terms').checked
     
     // Instan. user
     const user = new User(firstName, lastName, email, password, repPassword, checkbox)
-    
-    
     // Instant. UI
     const ui = new UI()
-
-
   // Validate
   if (
     firstName === '' || lastName === '' || email === '' || password === '' || repPassword === '') {
     // Error alert
-    ui.showAlert('All fields must be filled in correctly!', 'error')
-
+    ui.showAlert('All fields must be filled in correctly!', '#errorMessage')
   } 
   else if (!validText(firstName)) { 
     ui.showAlert(
-      'First name incorrect! Set a valid first name!', 'error')
+      'First name incorrect! Set a valid first name!', '#errorMessage')
 
   } else if (!validText(lastName)) { 
-    ui.showAlert('Last name incorrect! Set a valid last name!', 'error')
+    ui.showAlert('Last name incorrect! Set a valid last name!', '#errorMessage')
 
-  } 
-  else if (!validEmail(email)) {
-    ui.showAlert('Email error! Set a valid email address!', 'error')
+  } else if (!validEmail(email)) {
+    ui.showAlert('Email error! Set a valid email address!', '#errorMessage')
 
   } else if (!validPassword(password)) {
     ui.showAlert(
-      'Password Error : min 6 letters, Upper and lower case letters and numbers.', 'error');
+      'Password Error : min 6 letters, Upper and lower case letters and numbers.', '#errorMessage');
 
   } else if (password !== repPassword) {
-    ui.showAlert('Password does not match, repeat password!', 'error')
+    ui.showAlert('Password does not match, repeat password!', '#errorMessage')
 
-  } 
-  else if (!document.getElementById('terms').checked) {
-    ui.showAlert('You most accept our terms and conditions!', 'error');
+  } else if (!document.getElementById('terms').checked) {
+    ui.showAlert('You most accept our terms and conditions!', '#errorMessage');
+
   } else {
     // ui.addUserToList(user);
     // add user to list
     // Success
     ui.showAlert('You are registered, congratulations!', 'success');
 
+    console.log(user, 'You are successfully registered, congratulations!')
     // setSuccess(input)
     //Clear fields
     ui.clearFields();
   }
-  console.log(user)
+
+  
 
   
   e.preventDefault()
